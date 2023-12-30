33b784d1b2f5639833cd1cad0d94212794cc34d8
mohammad ghobadi <mohammadrezaghobadi.1383@gmail.com>
2023-12-30 10:31:06 +0330

    Finish the project

--- routes/web.php
+++ routes/web.php
@@ -15,33 +15,30 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::middleware('guest')->group(function () {
+    Route::get('/', [HomeController::class, 'create'])
+        ->name('home');
+});
 
-Route::get('/', [HomeController::class, 'create'])->name('home');
+Route::middleware('auth')->group(function () {
+    Route::get('dashboard', [TaskController::class, 'create'])
+        ->name('dashboard');
+    Route::post('tasks', [TaskController::class, 'store'])
+        ->name('task.store');
 
-Route::get('dashboard', [TaskController::class, 'create'])
-    ->name('dashboard')
-    ->middleware('auth');
-Route::post('tasks', [TaskController::class, 'store'])
-    ->name('task.store')
-    ->middleware('auth');;
+    Route::get('tasks', [TaskController::class, 'index'])
+        ->name('task.index');
+    Route::get('tasks/{task}', [TaskController::class, 'show'])
+        ->name('task.show');
 
-Route::get('tasks', [TaskController::class, 'index'])
-    ->name('task.index')
-    ->middleware('auth');
-Route::get('tasks/{task}', [TaskController::class, 'show'])
-    ->name('task.show')
-    ->middleware('auth');
+    Route::get('tasks/{task}/edit', [TaskController::class, 'edit'])
+        ->name('task.edit');
+    Route::patch('tasks/{task}', [TaskController::class, 'update'])
+        ->name('task.update');
 
-Route::get('tasks/{task}/edit', [TaskController::class, 'edit'])
-    ->name('task.edit')
-    ->middleware('auth');
-Route::patch('tasks/{task}', [TaskController::class, 'update'])
-    ->name('task.update')
-    ->middleware('auth');
-
-Route::delete('tasks/{task}', [TaskController::class, 'delete'])
-    ->name('task.delete')
-    ->middleware('auth');
+    Route::delete('tasks/{task}', [TaskController::class, 'delete'])
+        ->name('task.delete');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -49,4 +46,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
